a2eb648de0c70380edf70fedee9d0643ddd78bc5
srpillai <srpillai@hotmail.com>
2020-12-14 19:55:15 -0600

    add class 5

---
+++ Studio/class-5/BalancedBrackets/BalancedBrackets.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BalancedBracketsNS
+{
+    public class BalancedBrackets
+    {
+        /**
+         * The function BalancedBrackets should return true if and only if
+         * the input string has a set of "balanced" brackets.
+         *
+         * That is, whether it consists entirely of pairs of opening/closing
+         * brackets (in that order), none of which mis-nest. We consider a bracket
+         * to be square-brackets: [ or ].
+         *
+         * The string may contain non-bracket characters as well.
+         *
+         * These strings have balanced brackets:
+         *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+         *
+         * While these do not:
+         *   "[LaunchCode", "Launch]Code[", "[", "]["
+         *
+         * parameter str - to be validated
+         * returns true if balanced, false otherwise
+        */
+        public static bool HasBalancedBrackets(String str)
+        {
+            int brackets = 0;
+            foreach (char ch in str.ToCharArray())
+            {
+                if (ch == '[')
+                {
+                    brackets++;
+                }
+                else if (ch == ']')
+                {
+                    brackets--;
+                }
+            }
+            return brackets == 0;
+        }
+    }
+}
---
+++ Studio/class-5/BalancedBracketsTests/BalancedBracketsTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BalancedBracketsNS;
+
+namespace BalancedBracketsTests
+{
+    [TestClass]
+    public class BalancedBracketsTests
+    {
+        // TODO: Add tests to this file.
+
+        [TestMethod]
+        public void EmptyTest()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
