2260c8e49b0304f47dae6f5c9337506f8a8bd3fb
srpillai <srpillai@hotmail.com>
2021-01-11 19:35:21 -0600

    class 10

---
+++ Studio/studio-10/SpaDay/Controllers/SpaController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpaDay.Controllers
+{
+    public class SpaController : Controller
+    {
+        public bool CheckSkinType(string skinType, string facialType)
+        {
+
+            if (facialType != "Microdermabrasion")
+            {
+                if (skinType == "oily" && facialType != "Rejuvenating")
+                {
+                    return false;
+                }
+                else if (skinType == "combination" && (facialType != "Rejuvenating" || facialType != "Enzyme Peel"))
+                {
+                    return false;
+                }
+                else if (skinType == "dry" && facialType != "Hydrofacial")
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/spa")]
+        public IActionResult Menu(string name, string skintype, string manipedi)
+        {
+            List<string> facials = new List<string>()
+            {
+                "Microdermabrasion", "Hydrofacial", "Rejuvenating", "Enzyme Peel"
+            };
+
+            List<string> appropriateFacials = new List<string>();
+
+            for (int i = 0; i < facials.Count; i++)
+            {
+                if (CheckSkinType(skintype, facials[i]))
+                {
+                    appropriateFacials.Add(facials[i]);
+                }
+            }
+
+            ViewBag.name = name;
+            ViewBag.skintype = skintype;
+            ViewBag.manipedi = manipedi.ToUpper();
+
+            ViewBag.appropriateFacials = appropriateFacials;
+            return View();
+        }
+
+
+    }
+}
---
+++ Studio/studio-10/SpaDay/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpaDay.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
