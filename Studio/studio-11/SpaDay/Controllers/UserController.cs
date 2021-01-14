155d6ccdd2772b531ad967e47e04c811a1549e31
srpillai <srpillai@hotmail.com>
2021-01-13 20:00:22 -0600

    add class 11 studio

---
+++ Studio/studio-11/SpaDay/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Render the form
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            // add form submission handling code here
+            if (newUser.Password == verify)
+            {
+                // Students may pass in just in the name of the user to the Index view.
+                // They only need to pass in the whole object to complete the bonus mission.
+                ViewBag.user = newUser;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.error = "Passwords do not match! Try again!";
+                ViewBag.userName = newUser.Username;
+                ViewBag.eMail = newUser.Email;
+                return View("Add");
+            }
+        }
+    }
+}
---
+++ Studio/studio-11/SpaDay/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        // Bonus Mission
+        public DateTime SignUpTime { get; }
+        
+        public User()
+        {
+            SignUpTime = DateTime.Now;
+        }
+
+        public User(string u, string e, string p)
+        {
+            Username = u;
+            Email = e;
+            Password = p;
+            SignUpTime = DateTime.Now;
+        }
+    }
+}
